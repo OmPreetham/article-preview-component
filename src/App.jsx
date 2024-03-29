c9d47db3c4113f20225295808c89d911bd4b539b
Om Preetham <ompreethambandi@gmail.com>
2024-03-29 02:58:02 -0500

    designed component except for share functionality

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import ArticlePreview from './components/ArticlePreview'
+
+const App = () => {
+  return (
+    <main>
+      <ArticlePreview />
+    </main>
+  )
+}
+export default App
---
+++ src/components/ArticlePreview.jsx
@@ -0,0 +1,55 @@
+const ArticlePreview = () => {
+  /* TODO: Style and Toggle functionality for share on hover/focus */
+
+  return (
+    <div className="wrapper">
+      <section>
+        <div className="layout-grid">
+          <div className="image-wrapper">
+            <img src="/images/drawers.jpg" alt="Drawers" />
+          </div>
+          <div className="layout-flex">
+            <div className="headline-wrapper">
+              <p className="heading">
+                Shift the overall look and feel by adding these wonderful
+                touches to furniture in your home
+              </p>
+              <p className="description">
+                Ever been in a room and felt like something was missing? Perhaps
+                it felt slightly bare and uninviting. I’ve got some simple tips
+                to help you make any room feel complete.
+              </p>
+            </div>
+            <div className="profile-wrapper">
+              <div className="profile-data">
+                <img
+                  src="/images/avatar-michelle.jpg"
+                  alt="Michelle Appleton"
+                />
+                <div className="profile-info">
+                  <p className="profile-name">Michelle Appleton</p>
+                  <p className="profile-date">28 Jun 2020</p>
+                </div>
+              </div>
+              <div className="share-wrapper">
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="15"
+                    height="13"
+                  >
+                    <path
+                      fill="#6E8098"
+                      d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+export default ArticlePreview
